f9c4fe6810404f152a33105b0ecc4bf29d5d47a7
frajolz <frajolz@gmail.com>
2020-07-29 12:42:52 -0300

    Inicial

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Burn - but in 60's style"}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
